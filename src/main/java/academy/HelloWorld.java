7652d5d9b5e4a560e45995b51216179f5a99c441
Daniel Senften <daniel@senften.org>
2019-02-12 14:12:41 +0100

    Ich habe etwas geändert.

--- src/main/java/academy/HelloWorld.java
+++ src/main/java/academy/HelloWorld.java
@@ -3,6 +3,6 @@ package academy;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello!");
     }
 }
